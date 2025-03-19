14c34b9986b4340f9c1479bbfef900da9b2572d6
Kunkkkkkkk <145437982+Kunkkkkkkk@users.noreply.github.com>
2025-03-19 17:41:48 +0800

    Site updated: 2025-03-19 17:41:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/13/macOS显示文件已损坏，但是在隐私设置里没有信任的方法/","2025/03/13/使用picgo用github部署图床/","2025/03/14/使用ssh反向隧道进行内网穿透（实例：open webUI）/","2025/03/14/AutoSSH/"];function toRandomPost(){
+var posts=["2025/03/13/macOS显示文件已损坏，但是在隐私设置里没有信任的方法/","2025/03/13/使用picgo用github部署图床/","2025/03/14/使用ssh反向隧道进行内网穿透（实例：open webUI）/","2025/03/14/AutoSSH/","2025/03/19/100个python库推荐/","2025/03/19/MP4转wav/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
